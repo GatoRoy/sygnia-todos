62d7ad73660898104eba097a03e13a6d9d6aa734
GatoRoy <roy_iphone@yahoo.com>
2023-06-06 16:12:57 +0300

    mock

---
+++ src/store/api/mockSygniaTodoTasks.ts
@@ -0,0 +1,16 @@
+import { ITodoTask } from "../types";
+
+export const mockSygniaTodoTasks: ITodoTask[] = [
+    {"priority":3,"created_at":"1686053575","id":"806bdf10-2705-435a-a2c7-ceea786e48a3","status":"incomplete","title":"New Task 01"},
+    {"priority":3,"created_at":"1686046760.0035505","id":"9de4c4a2-4b1d-4eb6-ac87-da47921f81b9","title":"Finish the report","status":"incomplete"},
+    {"priority":2,"created_at":"1686046760.0035572","id":"c40a7f13-a010-44f3-9141-ec9e6916c69a","title":"Prepare for the meeting","status":"incomplete"},
+    {"priority":2,"created_at":"1686053591","id":"2c3816c8-fdf0-4a52-8c4a-0437727fffe6","status":"incomplete","title":"New Task 01"},
+    {"priority":2,"created_at":"1686046760.0035632","id":"1bc30bc8-dc45-4dd4-8d36-9a302597209f","title":"Book a restaurant","status":"incomplete"},
+    {"priority":2,"created_at":"1686046760.0035431","id":"dfdc1945-d6ec-4c0b-96af-88200fac4e76","title":"Call mom","status":"incomplete"},
+    {"priority":1,"created_at":"1686046760.0035696","id":"47184847-285b-49b4-b249-c989f7d6a76f","title":"Send an email to John","status":"complete"},
+    {"priority":1,"created_at":"1686053584","id":"e815dda8-1613-4c3e-bffd-37d94355f8b0","status":"complete","title":"New Task 01"},
+    {"priority":1,"created_at":"1686053591","id":"84e7d337-2a8d-4d13-b721-bbe19a72ad6a","title":"New Task 01","status":"incomplete"},
+    {"priority":1,"created_at":"1686046760.0035126","id":"8a747fb9-ac09-4972-83cc-3c6370ec8a84","title":"Buy groceries","status":"incomplete"},
+    {"priority":1,"created_at":"1686053584","id":"fd502a14-9f56-431f-b54e-269d4214594c","title":"New Task 01","status":"incomplete"},
+    {"priority":1,"created_at":"1686053575","id":"37721f24-a5f3-40a9-b2a0-5445eddacb0c","title":"New Task 01","status":"incomplete"}
+];
